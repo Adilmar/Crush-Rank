0cf38d2513a1e905aaabada10fe5e361753203ab
Adilmar <adilmarcoelho@hotmail.com>
2017-02-21 10:47:04 -0300

    final app volley

--- app/src/main/java/com/fenix/crushrank/Main.java
+++ app/src/main/java/com/fenix/crushrank/Main.java
@@ -1,13 +1,43 @@
 package com.fenix.crushrank;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Main extends AppCompatActivity {
+
+    EditText nome;
+    EditText nome2;
+
+    public String usuario;
+    public String parceiro;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nome   = (EditText)findViewById(R.id.nome);
+        nome2   = (EditText)findViewById(R.id.crush);
+
+
+
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                usuario = nome.getText().toString();
+                parceiro = nome2.getText().toString();
+
+                Intent it = new Intent(Main.this, MainActivity.class);
+                it.putExtra("my_name", usuario);
+                it.putExtra("my_name2",parceiro);
+                startActivity(it);
+            }
+        });
     }
 }
--- app/src/main/java/com/fenix/crushrank/MainActivity.java
+++ app/src/main/java/com/fenix/crushrank/MainActivity.java
@@ -3,9 +3,11 @@ package com.fenix.crushrank;
 /**
  * Created by adilmar on 10/02/17.
  */
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
@@ -26,19 +28,36 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
+import static android.R.attr.rating;
+import static com.fenix.crushrank.R.id.ratingBar;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    String JsonURL="https://love-calculator.p.mashape.com/getPercentage?fname=Adilmar&sname=Adrielly";
+    //String JsonURL="https://love-calculator.p.mashape.com/getPercentage?fname=Adilmar&sname=Adrielly";
     TextView results;
     //String JsonURL = "https://raw.githubusercontent.com/ianbar20/JSON-Volley-Tutorial/master/Example-JSON-Files/Example-Object.JSON";
     String data = "";
     RequestQueue requestQueue;
 
+
+    private RatingBar ratingBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.load);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("my_name");
+        String parceiro=intent.getStringExtra("my_name2");
+
+        String JsonURL="https://love-calculator.p.mashape.com/getPercentage?fname="+name+"&sname="+parceiro;
+        Log.d("xxxxxxxxxxxxxxxxxxx",name);
+
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        //ratingBar.setRating(3);
+        ratingBar.setFocusable(false);
 
         requestQueue = Volley.newRequestQueue(this);
 
@@ -58,7 +77,33 @@ public class MainActivity extends AppCompatActivity {
                             String porcentagem = response.getString("percentage");
                             String resultado = response.getString("result");
 
-                            data += "\nName: " + nome + "\n" +
+
+                            int por = Integer.parseInt(porcentagem);
+                            por = por / 10;
+
+                            ratingBar.setRating(por);
+
+                            if(resultado.equals("Not a good choice.")){
+
+                                resultado = "Não é uma boa escolha";
+                            }
+
+                            if(resultado.equals("Can choose someone better.")){
+
+                                resultado = "Você pode escolher alguém melhor";
+                            }
+
+
+                            if(resultado.equals("All the best!")){
+                                resultado = "Bem sucedidos!";
+                            }
+
+                            if (resultado.equals("Congratulations! Good choice")) {
+
+                                resultado = "Parabéns, excelente escolha";
+                            }
+
+                            data += "\nNome: " + nome + "\n" +
                                     "Crush : " + crush+"\n"+
                                     "Porcentagem: "+ porcentagem+"%"+ "\n"+
                                     "Resultado:"+ resultado;
